abfa502650facbd72eb1e686885bfb1c111a2615
Michael Welsh <brohemian1989@gmail.com>
2016-12-07 14:10:54 -0500

    added input boxes, added function to attempt. Added click.attempt to enter function

--- main.js
+++ main.js
@@ -28,14 +28,37 @@ enter = function() {
 	
 	var player1 = new User ($("#username").val(), 100, 3);
 	console.log(player1);
-	$(".newName").html("Hi " + player1.name);
-	$(".information").html("The goal of this game is Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga a nemo accusantium ipsum animi esse porro quisquam, quod, asperiores nam obcaecati possimus itaque repudiandae expedita voluptate. Explicabo voluptates culpa dolores!!! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea necessitatibus eligendi, cupiditate illo eos iusto soluta rem, excepturi quo, consectetur cumque repellat magnam consequuntur, velit voluptates placeat omnis non vitae.");
+		$(".newName").html("Hi " + player1.name);
+		$(".information").html("The goal of this game is Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga a nemo accusantium ipsum animi esse porro quisquam, quod, asperiores nam obcaecati possimus itaque repudiandae expedita voluptate. Explicabo voluptates culpa dolores!!! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea necessitatibus eligendi, cupiditate illo eos iusto soluta rem, excepturi quo, consectetur cumque repellat magnam consequuntur, velit voluptates placeat omnis non vitae.");
+		$(".charInfo").html("Your have " + player1.points + " points and " + player1.guesses + " guesses");
+		$("#attempt").click(attempt);
 
 
 }
 
 $("#submit").click(enter);
 
+
+
+
+// test to see if game works correctly.
+
+
+attempt = function() {
+
+if ($("#l1").val() === "M") {
+	User.points = + 5;
+	console.log("It Works!");
+
+} else {
+	console.log("It Doesnt!")
+}
+
+}
+
+
+
+// $("#attempt").click(attempt);
 
 
 
