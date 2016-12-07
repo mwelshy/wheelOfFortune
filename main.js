58748633cebf9bdb595c8b5a0323d644c0b164fe
Michael Welsh <brohemian1989@gmail.com>
2016-12-07 12:21:20 -0500

    enabled user to create their own character

--- main.js
+++ main.js
@@ -1,9 +1,53 @@
-user = function(name, points, guesses) {
+$( document ).ready(function() {
+
+
+
+
+
+
+
+//  create the user constructor
+
+User = function(name, points, guesses) {
 	this.name = name;
 	this.points = points;
 	this.guesses = guesses;
 
 }
+
+
+
+
+
+//  function to add a new user
+enter = function() {
+
+	
+	var player1 = new User ($("#username").val(), 100, 3);
+	console.log(player1);
+
+
+}
+
+$("#submit").click(enter);
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
