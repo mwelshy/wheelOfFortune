613df91b9d04f935174eabcb2231e3c9ae6039a7
Michael Welsh <brohemian1989@gmail.com>
2016-12-07 15:12:56 -0500

    Added guessing functionality. Stored player1 variable globally by putting it outside of documentready

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+// var player1;
+
+
 $( document ).ready(function() {
-
-
+var player1;
 
 
 
@@ -21,15 +23,14 @@ User = function(name, points, guesses) {
 
 
 
-
-//  function to add a new user and give description of the game they are about to play.
+//  function to add a new user and give description of the game they are about to play. Add click event to attempt button.
 enter = function() {
 
 	
-	var player1 = new User ($("#username").val(), 100, 3);
+	player1 = new User ($("#username").val(), 100, 20);
 	console.log(player1);
 		$(".newName").html("Hi " + player1.name);
-		$(".information").html("The goal of this game is Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga a nemo accusantium ipsum animi esse porro quisquam, quod, asperiores nam obcaecati possimus itaque repudiandae expedita voluptate. Explicabo voluptates culpa dolores!!! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea necessitatibus eligendi, cupiditate illo eos iusto soluta rem, excepturi quo, consectetur cumque repellat magnam consequuntur, velit voluptates placeat omnis non vitae.");
+		$(".information").html("The goal of this game is to guess the name! A name has been generated and its your job to figure out what that name actually is. You have a certain amount of attempts at this game so choose wisely!");
 		$(".charInfo").html("Your have " + player1.points + " points and " + player1.guesses + " guesses");
 		$("#attempt").click(attempt);
 
@@ -41,17 +42,27 @@ $("#submit").click(enter);
 
 
 
+
 // test to see if game works correctly.
 
 
 attempt = function() {
 
 if ($("#l1").val() === "M") {
-	User.points = + 5;
 	console.log("It Works!");
+	player1.points += 5;
+	player1.guesses -= 1;
+	$(".charInfo").html("You Have " + player1.points + " points." + "and " + player1.guesses + " guesses");
 
 } else {
 	console.log("It Doesnt!")
+	$(".charInfo").html("You Have " + player1.points + " points." + "and " + player1.guesses + " guesses");
+	player1.points -= 5;
+}
+
+if(player1.points === 0) {
+	$("body").hide();
+	console.log("Game Over");
 }
 
 }
